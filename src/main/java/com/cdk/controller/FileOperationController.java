09def3ad4cf4cd76e295a3373c54931ee2e246f0
Mohanty, Nirmal <Nirmal.Mohanty@cdk.com>
2018-07-17 02:04:37 +0530

    file operation using multithreadin and lambda expresssions

--- src/main/java/com/cdk/controller/FileOperationController.java
+++ src/main/java/com/cdk/controller/FileOperationController.java
@@ -6,16 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cdk.service.FileReadAndWriteSource;
+import com.cdk.service.FileReadAndWriteService;
 
 @RestController
 public class FileOperationController {
     @Autowired
-    FileReadAndWriteSource fileService;
+    FileReadAndWriteService fileService;
+
+    @GetMapping("/readFile")
+    public StringBuilder readFile() throws IOException {
+	return fileService.readFile();
+    }
 
     @GetMapping("/fileOperation")
     public void fileReadWrite() throws IOException {
 	fileService.fileReadWrite();
     }
 
+    @GetMapping("/read1Write2")
+    public void read1Write2() {
+	fileService.read1Write2();
+    }
+
+    @GetMapping("/read2Write1")
+    public void read2Write1() {
+	fileService.read2Write1();
+    }
 }
