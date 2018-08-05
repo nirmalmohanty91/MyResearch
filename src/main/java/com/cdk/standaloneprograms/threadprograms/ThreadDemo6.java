112cb0a59bd4ffef33ea8e433ba70f89839c126c
Mohanty, Nirmal <Nirmal.Mohanty@cdk.com>
2018-08-05 23:49:40 +0530

    formatted

--- src/main/java/com/cdk/standaloneprograms/threadprograms/ThreadDemo6.java
+++ src/main/java/com/cdk/standaloneprograms/threadprograms/ThreadDemo6.java
@@ -2,14 +2,18 @@ package com.cdk.standaloneprograms.threadprograms;
 
 import java.util.stream.IntStream;
 
-//Implementing threading using lambda 
+// Implementing threading using lambda
 public class ThreadDemo6 {
-    public static void main(String[] args) {
-	// Lambda Runnable
-	Runnable task1 = () -> IntStream.range(1, 6).forEach(n -> {
-	    System.out.println(Thread.currentThread().getName() + ": " + n);
-	});
-	// Starting a thread
-	new Thread(task1).start();
-    }
+  public static void main(String[] args) {
+    // Lambda Runnable
+    Runnable task1 =
+        () ->
+            IntStream.range(1, 6)
+                .forEach(
+                    n -> {
+                      System.out.println(Thread.currentThread().getName() + ": " + n);
+                    });
+    // Starting a thread
+    new Thread(task1).start();
+  }
 }
--- src/main/java/com/cdk/utils/SortLastNameComparator.java
+++ src/main/java/com/cdk/utils/SortLastNameComparator.java
@@ -11,7 +11,4 @@ public class SortLastNameComparator implements Comparator<Person> {
   public int compare(Person o1, Person o2) {
     return o1.getLastName().compareTo(o2.getLastName());
   }
-
-
-
 }
