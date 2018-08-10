56425b3a01683ca63b9312ad183af7ce37bc66e8
Mohanty, Nirmal <Nirmal.Mohanty@cdk.com>
2018-08-10 22:40:57 +0530

    more addition

---
+++ src/main/java/com/cdk/standaloneprograms/threadprograms/Test.java
@@ -0,0 +1,24 @@
+package com.cdk.standaloneprograms.threadprograms;
+
+public class Test {
+
+    
+    
+    public static String adoptPriceFromDMS(String price) {
+	    String result = Float.toString(Float.parseFloat(price) / 100);
+	    return result;
+	  }
+    
+    
+    public static String adoptPriceFromUI(String price) {
+	    String result = Integer.toString(Math.round((Float.parseFloat(price) * 100)));
+	    return result;
+	  }
+    
+    public static void main(String[] args) {
+	System.out.println(Test.adoptPriceFromDMS("5555"));
+	System.out.println(Test.adoptPriceFromUI("16.59"));
+
+    }
+
+}
