f8ac87a705e7f528a5d24ab56c1da6e09ef694b5
Mohanty, Nirmal <Nirmal.Mohanty@cdk.com>
2018-11-23 15:51:03 +0530

    refactored

---
+++ src/main/java/com/nirmal/standaloneprograms/Java8concepts/$6Java8Interface2.java
@@ -0,0 +1,7 @@
+package com.nirmal.standaloneprograms.Java8concepts;
+
+public interface $6Java8Interface2 {
+  static void main(String[] args) {
+    System.out.println("Yo!! main inside interface. Fucking awesome!!");
+  }
+}
--- src/main/java/com/nirmal/standaloneprograms/Java8concepts/source1/$6Java8Interface2.java
+++
@@ -1,7 +0,0 @@
-package com.nirmal.standaloneprograms.Java8concepts.source1;
-
-public interface $6Java8Interface2 {
-  static void main(String[] args) {
-    System.out.println("Yo!! main inside interface. Fucking awesome!!");
-  }
-}
